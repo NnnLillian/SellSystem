dd23f72b524ae4f9bb95f00864dd55e9486a508b
Lillian <weijingwen35@foxmail.com>
2018-12-11 21:07:52 +0800

    Build BuyerProduct Controller & Define the outermost layer of Get JSON

---
+++ src/main/java/com/imooc/VO/ResultVO.java
@@ -0,0 +1,15 @@
+package com.imooc.VO;
+//  http要求返回的最外层对象
+
+import lombok.Data;
+
+@Data
+public class ResultVO<T> {
+
+//    错误码
+    private Integer code;
+//    提示信息
+    private String msg;
+//    具体内容
+    private T data;
+}
