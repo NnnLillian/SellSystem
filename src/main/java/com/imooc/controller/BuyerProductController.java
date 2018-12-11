f3d0921198708735361d5aab5f6401760a0c4e1d
Lillian <weijingwen35@foxmail.com>
2018-12-11 21:30:29 +0800

    Build BuyerProduct Controller Without content

--- src/main/java/com/imooc/controller/BuyerProductController.java
+++ src/main/java/com/imooc/controller/BuyerProductController.java
@@ -1,9 +1,13 @@
 package com.imooc.controller;
 
+import com.imooc.VO.ProductInfoVO;
+import com.imooc.VO.ProductVO;
 import com.imooc.VO.ResultVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/buyer/product")
@@ -12,6 +16,15 @@ public class BuyerProductController {
     @GetMapping("/list")
     public ResultVO list(){
         ResultVO resultVO = new ResultVO();
+
+        ProductVO productVO = new ProductVO();
+
+        ProductInfoVO productInfoVO = new ProductInfoVO();
+
+        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
+        resultVO.setData(Arrays.asList(productVO));
+        resultVO.setData(productVO);
+
         return resultVO;
     }
 }
