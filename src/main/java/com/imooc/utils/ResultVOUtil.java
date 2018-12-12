75da5836f0219bd69667bd56043100c66d48e037
Lillian <weijingwen35@foxmail.com>
2018-12-12 19:10:52 +0800

    完善ResultVO所有可能的返回方法

--- src/main/java/com/imooc/utils/ResultVOUtil.java
+++ src/main/java/com/imooc/utils/ResultVOUtil.java
@@ -10,4 +10,15 @@ public class ResultVOUtil {
         resultVO.setCode(0);
         return resultVO;
     }
+
+    public static ResultVO success(){
+        return success(null);
+    }
+
+    public static ResultVO error(Integer code, String msg){
+        ResultVO resultVO = new ResultVO();
+        resultVO.setCode(code);
+        resultVO.setMsg(msg);
+        return resultVO;
+    }
 }
