34e10a33ba1897c302a7e6c48e8c6dc62060d9f9
Lillian <weijingwen35@foxmail.com>
2019-07-09 17:09:27 +0800

    2019-07-09

---
+++ src/main/java/com/imooc/dataobject/OrderMaster.java
@@ -0,0 +1,10 @@
+package com.imooc.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+public class OrderMaster {
+}
--- src/main/java/com/imooc/dataobject/ProductCategory.java
+++ src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Date;
 
 //该注解将数据库映射为对象
 @Entity
