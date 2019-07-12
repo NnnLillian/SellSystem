5a2c45f667c96b03d7b19f9e8163c46a3692bcc8
Lillian <weijingwen35@foxmail.com>
2019-07-12 21:06:15 +0800

    新建异常处理以及返回给前端的枚举异常消息

---
+++ src/main/java/com/imooc/exception/SellException.java
@@ -0,0 +1,19 @@
+package com.imooc.exception;
+
+import com.imooc.enums.ResultEnum;
+
+import javax.persistence.criteria.CriteriaBuilder;
+
+/**
+ * @author: Lillian
+ * @create: 2019-07-12 20:55
+ */
+public class SellException extends RuntimeException {
+    private Integer code;
+
+    public SellException(ResultEnum resultEnum) {
+        super(resultEnum.getMessage());
+
+        this.code = resultEnum.getCode();
+    }
+}
